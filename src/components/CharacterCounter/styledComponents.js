b22aacfbda44370b44b8cf00a2af1fca1984ac13
Eralingappa <nxtwave@ip-10-2-0-34.ap-south-1.compute.internal>
2024-06-21 16:54:43 +0000

    first commit

---
+++ src/components/CharacterCounter/styledComponents.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+display:flex;
+`
+export const LeftPanel = styled.div`
+background-color:#ffc533;
+height:100vh;
+width:50%;
+padding:10px;
+`
+export const InfoCardContainer = styled.div`
+background-color:#ffbf1f;
+padding:20px;
+`
+export const Info = styled.h1`
+color:#334155;
+font-weight:'bold';
+font-size:32px;
+`
+export const RightPanel = styled.div`
+background-color:#0f172a;
+width:50%;
+padding:20px;
+`
+export const CounterHeading = styled.h1`
+
+color:#ffbf1f;
+font-weight:'bold';
+font-size:32px;
+font-family:'Roboto';
+text-align:center;
+margin-bottom:60px;
+`
+export const AddInputContainer = styled.form`
+ display:flex;
+ `
+export const Input = styled.input`
+ 
+ height:32px;
+ width:100%;
+ border-radius:2px;
+ color:#475569;
+ `
+export const AddInputButton = styled.button`
+ 
+ height:32px;
+ background-color:#ffc533;
+ color:#334155;
+ padding:10px 20px 10px 20px
+ `
+export const UserInputsList = styled.ul`
+ margin-top:50px;
+ 
+ `
+export const EmptyImage = styled.img`
+ height:400px;
+ width:100%;
+ padding:40px;
+ margin-top:60px;
+ `
